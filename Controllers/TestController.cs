2a7c6697b471af87726f969a0e4c00823df6cc48
Viacheslav Titov <vtitov@ibs.ru>
2020-03-30 23:17:06 +0300

    Initial commit

---
+++ Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.Serialization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Integration1C.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpPost, Route("v1")]
+        [Consumes("application/json", "application/xml")]
+        [Produces("application/json", "application/xml")]
+        public TestRequest Test([FromBody] TestRequest data)
+        {
+            return data;
+        }
+    }
+
+    [DataContract]
+    public class TestRequest
+    {
+        [DataMember]
+        public Guid TestGuid { get; set; }
+        [DataMember]
+        public string TestString { get; set; }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace Integration1C.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers().AddXmlSerializerFormatters();
+            services.AddSwaggerGen(c =>
+            {
+                c.DescribeAllParametersInCamelCase();
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Neocase <-> 1C Integration", Version = "v1" });
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Neocase <-> 1C Integration");
+            });
+        }
+    }
+}
